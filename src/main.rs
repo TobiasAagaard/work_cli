c247692652908c8fcfe67f672c046206e50b81ab
TobiasAagaard <Tobias909@live.dk>
2025-05-09 20:58:07 +0200

    layout for main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,38 @@
 use inquire::Select;
 
 fn main() {
-    let options = vec![
-        "Start tracking time",
-        "Add new task",
-        "View tasks",
-        "Exit",
-    ];
+    loop {
+        let options: Vec<&'static str> = vec![
+            "Start tracking time",
+            "Add new task",
+            "View tasks",
+            "Exit",
+        ];
 
-    let ans = Select::new("What do you want to do?", options)
-        .prompt();
-
-    match ans {
-        Ok(choice) => println!("You chose: {}", choice),
-        Err(err) => println!("Error: {}", err),
+        let ans: Result<&'static str, inquire::InquireError> = Select::new("What do you want to do?", options).prompt();
+     
+        match ans {
+            Ok("Start tracking time") => start_tracking(),
+            Ok("Add new task") => add_task(),
+            Ok("View tasks") => view_tasks(),
+            Ok("Exit") => break,
+            Ok(_) => println!("Invalid selection."),
+            Err(_) => {
+                println!("Something went wrong with the prompt.");
+                break;
+            }
+        }
     }
+}
+
+fn start_tracking() {
+    println!("Started tracking time...");
+}
+
+fn add_task() {
+    println!("Add a new task...");
+}
+
+fn view_tasks() {
+    println!("Viewing tasks...");
 }
